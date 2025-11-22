57cbe30db7156cf64a3ef9cdac61ef56a0d06c9b
eyeej <eyee.joy@gmail.com>
2025-11-22 18:44:20 +1100

    updates to js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       </main>
       <footer>
         <small>
-        <div class="text-center">This Dictionary was Coded by Emma Yee-Joy is <a href="https://github.com/eyeej/shecodes-dictionary">open-sourced</a> on Github and is hosted on <a href="https://shecodes-dictionary-io.netlify.app/">Netlify</a></div>
+        <div className="text-center">This Dictionary was Coded by Emma Yee-Joy is <a href="https://github.com/eyeej/shecodes-dictionary">open-sourced</a> on Github and is hosted on <a href="https://shecodes-dictionary-io.netlify.app/">Netlify</a></div>
         </small>
         </footer>
     </div>
